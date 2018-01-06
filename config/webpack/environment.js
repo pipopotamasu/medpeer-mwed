8c07e0a0a4e5f040441a80c2849f4f64abacf40e
pipopotamasu <ec0156hx@gmail.com>
2018-01-06 20:31:06 +0900

    add chunk setting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,29 @@
+// config/webpack/environment.js
+
 const { environment } = require('@rails/webpacker')
+
+// NOTE: CommonChunkPluginここから
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1
+    }
+  })
+)
+
+environment.plugins.append(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity
+  })
+)
+
+// NOTE: ここまで
 
 module.exports = environment
