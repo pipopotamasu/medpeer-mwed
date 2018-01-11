bfb227ecb30334ccd3be458839e022043ad1926b
pipopotamasu <ec0156hx@gmail.com>
2018-01-11 10:12:35 +0900

    jqeury pattern

--- app/javascript/packs/csrf/hoges/index.js
+++ app/javascript/packs/csrf/hoges/index.js
@@ -1,10 +1,20 @@
-import axios from 'axios'
+// import axios from 'axios'
+import $ from 'jquery'
 import { csrfToken } from 'rails-ujs'
 
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
-axios.post('/csrf/hoges/post').then((res) => {
-  console.log(res)
-}).catch((e) => {
-  console.log(e)
+$.ajax({
+  url: '/csrf/hoges/post',
+  method: 'POST',
+  dataType: 'json',
+  success: (response) => {
+    console.log(response);
+  }
 });
+
+// axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+//
+// axios.post('/csrf/hoges/post').then((res) => {
+//   console.log(res)
+// }).catch((e) => {
+//   console.log(e)
+// });
