d9987e963b4af70b38342d0fcc989605ce3844e1
pipopotamasu <ec0156hx@gmail.com>
2018-01-11 10:17:11 +0900

    add csrf into jquery

--- app/javascript/packs/csrf/hoges/index.js
+++ app/javascript/packs/csrf/hoges/index.js
@@ -2,6 +2,12 @@
 import $ from 'jquery'
 import { csrfToken } from 'rails-ujs'
 
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': csrfToken()
+  }
+})
+
 $.ajax({
   url: '/csrf/hoges/post',
   method: 'POST',
